df42ec7dd7e9cd30c316b3913b91820575c30ac6
saiful Islam <saifulmeraz897@gmail.com>
2021-12-19 20:58:35 +0600

    practice var and data type

---
+++ var_and_dataType.js
@@ -0,0 +1,42 @@
+//Two types of data in javaScript 
+//1.Primitive
+// --Number
+var aNumber =20;
+console.log(aNumber);
+// --String
+var name ="Saiful Islam"
+console.log(name);
+// --Boolean
+var pakhi =true
+console.log(pakhi)
+// --undefined
+var fullName;
+console.log(fullName);
+// --null
+var age =null;
+console.log(age);
+//2.Non Primitive
+// --Array
+var productList=['apple','mango','banana'];
+console.log(productList[0]);
+//--loop
+// ---for
+for(var i=0;i<10;i++){
+    console.log(i)
+}
+// ---while
+while( i<10){
+console.log(i);
+i++
+}
+// ---do while
+do{
+    i++;
+    console.log(i);
+}
+while(i<10)
+//--function
+function myFunction(){
+    return console.log("This is my life whatever i doing my life")
+}
+myFunction()
