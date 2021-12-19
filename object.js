1b8ed970295f1a90707775f6b40054db06be87d3
saiful Islam <saifulmeraz897@gmail.com>
2021-12-20 00:38:37 +0600

    practice object

---
+++ object.js
@@ -0,0 +1,12 @@
+var person ={
+    name:"saiful",
+    age:25,
+    cgpa:3.08,
+    hobbies:['cricket','swimming'],
+    objective:function(){
+        console.log("seeking for a fronted developer job")
+    }
+}
+console.log(person.age)
+console.log(person.hobbies[1])
+person.objective()
