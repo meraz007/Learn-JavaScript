52d3e8adc622ddcb9623674775e427f538465f4a
saiful Islam <saifulmeraz897@gmail.com>
2021-12-20 00:01:55 +0600

    array practice

---
+++ array1.js
@@ -0,0 +1,21 @@
+var arr =new Array(1,2,3,4,5)
+var newArray=[];
+for(var i=0;i<arr.length;i++){
+    var anotherArray=newArray.push(arr[i]);
+}
+console.log(newArray) 
+var arr2=['rahim','karim','rony','sakib'];
+arr2.push('razauls');
+console.log(arr2);
+arr2.shift()
+console.log(arr2)
+arr2.unshift('jakir')
+console.log(arr2)
+arr2.splice(0,2)
+console.log(arr2)
+var arr3 =[1,2,3,4,5]
+var sl=arr3.slice(1,3)
+console.log(sl)
+console.log(arr3)
+console.log(arr2)
+
