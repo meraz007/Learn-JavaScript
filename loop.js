e091c4b72714b5e5dadd4d20728eee7dd05c976b
saiful Islam <saifulmeraz897@gmail.com>
2021-12-19 22:47:21 +0600

    practice loop

---
+++ loop.js
@@ -0,0 +1,20 @@
+//for loop
+var num=10;
+var i;
+for(i=0;i<num;i++){
+    console.log(i);
+}
+//do while 
+var text = "";
+var j = 0;
+do {
+  text += "The number is " + j;
+  j++;
+}
+while (j < 5);
+//while
+var num2=10;
+while(num2<=10){
+    console.log(num2);
+    num2++;
+}
