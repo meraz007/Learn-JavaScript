f7ebbcaae0c37a1536400229361c4a3305180917
saiful Islam <saifulmeraz897@gmail.com>
2021-12-19 21:47:22 +0600

    Practice Conditional statement

---
+++ conditional_statement.js
@@ -0,0 +1,32 @@
+var a=10;
+var b=20;
+var c=30;
+if (a>b && a>c ){
+    console.log(a)
+}
+else if (b>a && b>c){
+    console.log(b)
+}else{
+    console.log(c)
+}
+
+//switch Case
+
+var aNumber ='4';
+
+switch(aNumber){
+    case '1':
+        console.log("Amit");
+        break;
+    case '2':
+        console.log("Aamin")
+        break;
+    case '3':
+        console.log("Rohan")
+        break;
+    case '4':
+        console.log("Saiful")
+        break;
+    default:
+        console.log("Rokon")
+}
