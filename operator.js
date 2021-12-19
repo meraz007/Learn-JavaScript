897a134aa4dac305edb2c3003d54326ee23f96c3
saiful Islam <saifulmeraz897@gmail.com>
2021-12-19 21:16:48 +0600

    Practice operator

---
+++ operator.js
@@ -0,0 +1,16 @@
+var a =10;
+var b=20;
+
+var c =a +b;
+var d = a*b;
+var e =a-b;
+var f =a /b;
+var g =a % 2;
+console.log(c,d,e,f,g)
+a++
+console.log(a);
+a--
+console.log(a);
+
+var j =(a>b)? "a is greater then b":"a is lessthen b";
+console.log(j)
